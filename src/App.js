0f8b5fe89100bd13f3ba04866233843cd1f945c7
devdruh <dev.drewragadio@gmail.com>
2023-10-27 21:12:37 -0400

    add theme cookie

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import logo from './logo.svg';
-import * as React from 'react';
-import './App.css';
+import { useState, useMemo } from 'react';
+import './assets/app.css';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
@@ -13,46 +13,52 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Grid, CssBaseline } from '@mui/material';
 import { ColorModeContext } from './utils/variables';
+import { CookiesProvider, useCookies } from 'react-cookie';
 
 function App() {
 
-  const [mode, setMode] = React.useState('light');
-  const colorMode = React.useMemo(
+  const [cookies, setCookie] = useCookies(['isDarkMode']);
+  const [mode, setMode] = useState('light');
+
+  const colorMode = useMemo(
     () => ({
       toggleColorMode: () => {
-        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+        setMode((prevMode) => prevMode === 'light' ? 'dark' : 'light');
+        setCookie('isDarkMode', mode);
       },
     }),
-    [],
+    [mode, setCookie],
   );
 
-  const defaultTheme = React.useMemo(
+  const defaultTheme = useMemo(
     () =>
       createTheme({
         palette: {
-          mode,
+          mode: cookies.isDarkMode === undefined ? 'light' : cookies.isDarkMode
         },
       }),
-    [mode],
+    [cookies.isDarkMode],
   );
 
   return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={defaultTheme}>
-        <Grid component="main">
-          <CssBaseline />
-          <BrowserRouter>
-            <Routes>
-              <Route path='/' element={<Layout/>}>
-                <Route index element={<Home />} />
-                <Route path='data-and-map' element={<DataAndMap />} />
-                <Route path='about' element={<About/>} />
-              </Route>
-            </Routes>
-          </BrowserRouter>
-        </Grid>
-      </ThemeProvider>
-    </ColorModeContext.Provider>
+    <CookiesProvider>
+      <ColorModeContext.Provider value={colorMode}>
+        <ThemeProvider theme={defaultTheme}>
+          <Grid component="main">
+            <CssBaseline />
+            <BrowserRouter>
+              <Routes>
+                <Route path='/' element={<Layout/>}>
+                  <Route index element={<Home />} />
+                  <Route path='data-and-map' element={<DataAndMap />}/>
+                  <Route path='about' element={<About/>} />
+                </Route>
+              </Routes>
+            </BrowserRouter>
+          </Grid>
+        </ThemeProvider>
+      </ColorModeContext.Provider>
+    </CookiesProvider>
   );
 }
 
